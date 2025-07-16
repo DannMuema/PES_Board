16d3011e555693936ab288cbf90e277cfd4e4e8f
Daniel Muema <dann.muemaa@gmail.com>
2025-07-16 12:34:26 +0000

    line_gimbal

--- src/main.cpp
+++ src/main.cpp
@@ -1,90 +1,127 @@
+// This merged code integrates both line following and gimbal control using IMU and SensorBar
+// Key components: DCMotor, SensorBar, IMU, Servo
+
 #include "mbed.h"
-
-// pes board pin map
 #include "PESBoardPinMap.h"
-
-// drivers
 #include "DebounceIn.h"
+#include "DCMotor.h"
+#include "SensorBar.h"
+#include "IMU.h"
+#include "Servo.h"
+#include <Eigen/Dense>
+#include <cmath>
 
-bool do_execute_main_task = false; // this variable will be toggled via the user button (blue button) and
-                                   // decides whether to execute the main task or not
-bool do_reset_all_once = false;    // this variable is used to reset certain variables and objects and
-                                   // shows how you can run a code segment only once
+#define M_PIf 3.14159265358979323846f
 
-// objects for user button (blue button) handling on nucleo board
-DebounceIn user_button(BUTTON1);   // create DebounceIn to evaluate the user button
-void toggle_do_execute_main_fcn(); // custom function which is getting executed when user
-                                   // button gets pressed, definition at the end
+bool do_execute_main_task = false;
+bool do_reset_all_once = false;
 
-// main runs as an own thread
-int main()
-{
-    // attach button fall function address to user button object
+DebounceIn user_button(BUTTON1);
+void toggle_do_execute_main_fcn() { do_execute_main_task = !do_execute_main_task; if (do_execute_main_task) do_reset_all_once = true; }
+
+int main() {
     user_button.fall(&toggle_do_execute_main_fcn);
-
-    // while loop gets executed every main_task_period_ms milliseconds, this is a
-    // simple approach to repeatedly execute main
-    const int main_task_period_ms = 20; // define main task period time in ms e.g. 20 ms, therefore
-                                        // the main task will run 50 times per second
-    Timer main_task_timer;              // create Timer object which we use to run the main task
-                                        // every main_task_period_ms
-
-    // led on nucleo board
+    const int main_task_period_ms = 20;
+    Timer main_task_timer;
     DigitalOut user_led(LED1);
-
-    // additional led
-    // create DigitalOut object to command extra led, you need to add an additional resistor, e.g. 220...500 Ohm
-    // a led has an anode (+) and a cathode (-), the cathode needs to be connected to ground via the resistor
     DigitalOut led1(PB_9);
+    DigitalOut enable_motors(PB_ENABLE_DCMOTORS);
 
-    // --- adding variables and objects and applying functions starts here ---
+    // Servo setup
+    Servo servo_roll(PB_D0), servo_pitch(PB_D1);
+    float servo_ang_min = 0.035f, servo_ang_max = 0.125f;
+    servo_roll.calibratePulseMinMax(servo_ang_min, servo_ang_max);
+    servo_pitch.calibratePulseMinMax(servo_ang_min, servo_ang_max);
+    float angle_range_min = -M_PIf/2.0f, angle_range_max = M_PIf/2.0f;
+    float normalised_angle_gain = 1.0f / M_PIf, normalised_angle_offset = 0.5f;
+    float roll_servo_width = 0.5f, pitch_servo_width = 0.5f;
 
-    // start timer
+    // IMU setup
+    ImuData imu_data;
+    IMU imu(PB_IMU_SDA, PB_IMU_SCL);
+    Eigen::Vector2f rp(0.0f, 0.0f);
+    float phi_hat = 0.0f, theta_hat = 0.0f;
+    float Ts = main_task_period_ms * 1.0e-3f, kp_roll = 3.0f; 
+    float kp_pitch = 5.0f;
+
+    // DCMotor setup
+    float voltage_max = 12.0f, gear_ratio = 100.0f, kn = 140.0f / 12.0f;
+    DCMotor motor_M1(PB_PWM_M1, PB_ENC_A_M1, PB_ENC_B_M1, gear_ratio, kn, voltage_max);
+    DCMotor motor_M2(PB_PWM_M2, PB_ENC_A_M2, PB_ENC_B_M2, gear_ratio, kn, voltage_max);
+    float d_wheel = 0.100f, b_wheel = 0.172f, bar_dist = 0.022f;
+    float r1_wheel = d_wheel / 2.0f, r2_wheel = d_wheel / 2.0f;
+    Eigen::Matrix2f Cwheel2robot;
+    Cwheel2robot << r1_wheel/2.0f, r2_wheel/2.0f, r1_wheel/b_wheel, -r2_wheel/b_wheel;
+
+    // SensorBar setup
+    SensorBar sensor_bar(PB_9, PB_8, bar_dist);
+    float angle = 0.0f, angular_vel = 0.0f, velocity = 0.0f;
+    float Kp_line = 2.0f, Kp_nl = 5.0f, gain = 2.0f;
+    float wheel_vel_max = 0.5f * M_PIf * motor_M2.getMaxPhysicalVelocity();
+
     main_task_timer.start();
 
-    // this loop will run forever
     while (true) {
         main_task_timer.reset();
 
-        // --- code that runs every cycle at the start goes here ---
+        imu_data = imu.getImuData();
 
         if (do_execute_main_task) {
+            user_led = 1; led1 = 1; enable_motors = 1;
+            if (!servo_roll.isEnabled()) servo_roll.enable();
+            if (!servo_pitch.isEnabled()) servo_pitch.enable();
 
-        // --- code that runs when the blue button was pressed goes here ---
+            float acc_x = imu_data.acc(0), acc_y = imu_data.acc(1), acc_z = imu_data.acc(2);
+            float gyro_x = imu_data.gyro(0), gyro_y = imu_data.gyro(1);
+            float phi_acc = atan2(acc_y, acc_z);
+            float theta_acc = atan2(-acc_x, acc_z);
 
-            // visual feedback that the main task is executed, setting this once would actually be enough
-            led1 = 1;
+            phi_hat += Ts * (gyro_x + kp_roll * (phi_acc - phi_hat));
+            theta_hat += Ts * (gyro_y + kp_pitch * (theta_acc - theta_hat));
+            rp(0) = phi_hat; rp(1) = theta_hat;
+
+            roll_servo_width = -normalised_angle_gain * rp(0) + normalised_angle_offset;
+            pitch_servo_width =  normalised_angle_gain * rp(1) + normalised_angle_offset;
+            if (angle_range_min <= rp(0) && rp(0) <= angle_range_max) servo_roll.setPulseWidth(roll_servo_width);
+            if (angle_range_min <= rp(1) && rp(1) <= angle_range_max) servo_pitch.setPulseWidth(pitch_servo_width);
+
+            if (sensor_bar.isAnyLedActive())
+                angle = sensor_bar.getAvgAngleRad();
+
+            if (angle >= 0.0f)
+                angular_vel = Kp_line * angle - Kp_nl * angle * std::abs(angle);
+            else
+                angular_vel = Kp_line * angle - Kp_nl * angle * std::abs(angle);
+
+            // velocity = wheel_vel_max * r1_wheel * exp(-gain * angle * angle);
+
+            velocity = wheel_vel_max * r1_wheel * std::max(0.0f, cos(angle));
+
+            // velocity = wheel_vel_max * r1_wheel * (1.0f / (1.0f + gain * angle * angle));
+
+
+            Eigen::Vector2f robot_coord = {velocity, angular_vel};
+            Eigen::Vector2f wheel_speed = Cwheel2robot.inverse() * robot_coord;
+
+            motor_M1.setVelocity(wheel_speed(0) / (2.0f * M_PIf));
+            motor_M2.setVelocity(wheel_speed(1) / (2.0f * M_PIf));
         } else {
-            // the following code block gets executed only once
             if (do_reset_all_once) {
                 do_reset_all_once = false;
-
-                // --- variables and objects that should be reset go here ---
-
-                // reset variables and objects
-                led1 = 0;
+                roll_servo_width = pitch_servo_width = 0.5f;
+                servo_roll.setPulseWidth(roll_servo_width);
+                servo_pitch.setPulseWidth(pitch_servo_width);
+                led1 = 0; enable_motors = 0;
             }
         }
 
-        // toggling the user led
+        printf("Roll = %.2f | Pitch = %.2f\n", rp(0), rp(1));
         user_led = !user_led;
 
-        // --- code that runs every cycle at the end goes here ---
-
-        // read timer and make the main thread sleep for the remaining time span (non blocking)
         int main_task_elapsed_time_ms = duration_cast<milliseconds>(main_task_timer.elapsed_time()).count();
         if (main_task_period_ms - main_task_elapsed_time_ms < 0)
-            printf("Warning: Main task took longer than main_task_period_ms\n");
+            printf("Warning: Main task took too long\n");
         else
             thread_sleep_for(main_task_period_ms - main_task_elapsed_time_ms);
     }
-}
-
-void toggle_do_execute_main_fcn()
-{
-    // toggle do_execute_main_task if the button was pressed
-    do_execute_main_task = !do_execute_main_task;
-    // set do_reset_all_once to true if do_execute_main_task changed from false to true
-    if (do_execute_main_task)
-        do_reset_all_once = true;
 }
